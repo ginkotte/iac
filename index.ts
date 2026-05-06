931c9ade5a67463c2c108f2f6eb2f9a8cc1c65d3
Gustavo Inkotte <gus.inkotte@gmail.com>
2026-05-06 18:34:32 -0300

    Remove second S3 Bucket

--- index.ts
+++ index.ts
@@ -7,13 +7,6 @@ const fistBucket = new aws.s3.Bucket("teste-inicial-iac-s3", {
     }
 });
 
-const secondBucket = new aws.s3.Bucket("teste-2-inicial-iac-s3", {
-    bucket: "teste-2-inicial-iac-s3",
-    tags: {
-        IAC: "true"
-    }
-});
-
 const ecr = new aws.ecr.Repository("teste-inicial-iac-ecr", {
     name: "teste-inicial-iac-ecr",
     imageTagMutability: "IMMUTABLE",
@@ -25,10 +18,6 @@ const ecr = new aws.ecr.Repository("teste-inicial-iac-ecr", {
 export const fistBucketName = fistBucket.id;
 export const fistBucketRegion = fistBucket.region;
 export const fistBucketArn = fistBucket.arn;
-
-export const secondBucketName = secondBucket.id;
-export const secondBucketRegion = secondBucket.region;
-export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
